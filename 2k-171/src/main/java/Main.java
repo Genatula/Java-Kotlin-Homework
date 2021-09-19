754c8d11a45ef9508239b6a9f2e1ea4a430acddf
ilnas <genatula@protonmail.com>
2021-09-19 16:36:07 +0300

    Write Main Test

--- 2k-171/src/main/java/Main.java
+++ 2k-171/src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         data = parser.parse(data);
         Number firstValue = Double.parseDouble(data.get(0));
         Number secondValue = Double.parseDouble(data.get(2));
-        String sign = (String) data.get(1);
+        String sign = data.get(1);
         Number result = calculator.calculate(firstValue, sign, secondValue);
 
         System.out.println(result);
--- 2k-171/src/test/java/MainTest.java
+++ 2k-171/src/test/java/MainTest.java
@@ -1,10 +1,42 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class MainTest {
 
     @Test
-    public void main() {
+    public void mainWithLessThanThreeArgs() {
+        String[] args = new String[]{"2", "+"};
+        Assert.assertThrows(IllegalArgumentException.class, () -> Main.main(args));
+    }
+
+    @Test
+    public void mainWithMoreThanThreeArgs() {
+        String[] args = new String[]{"2", "+", "2", "+"};
+        Assert.assertThrows(IllegalArgumentException.class, () -> Main.main(args));
+    }
+
+    @Test
+    public void mainWithoutFirstNumber() {
+        String[] args = new String[]{"+", "2", "2"};
+        Assert.assertThrows(IllegalArgumentException.class, () -> Main.main(args));
+    }
+
+    @Test
+    public void mainWithoutSecondNumber() {
+        String[] args = new String[]{"2", "2", "+"};
+        Assert.assertThrows(IllegalArgumentException.class, () -> Main.main(args));
+    }
+
+    @Test
+    public void mainDivisionByZero() {
+        String[] args = new String[]{"2", "/", "0"};
+        Assert.assertThrows(ArithmeticException.class, () -> Main.main(args));
+    }
+
+    @Test
+    public void mainUnsupportedOperation() {
+        String[] args = new String[]{"2", "2", "2"};
+        Assert.assertThrows(IllegalArgumentException.class, () -> Main.main(args));
     }
 }
