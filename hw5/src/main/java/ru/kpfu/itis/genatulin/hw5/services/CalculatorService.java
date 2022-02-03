c4c77ce0d92bd603da36bfab8f93b8af0a24b0b2
ilnas <genatula@protonmail.com>
2022-02-03 22:34:04 +0300

    Add repository functionality to service

--- hw5/src/main/java/ru/kpfu/itis/genatulin/hw5/services/CalculatorService.java
+++ hw5/src/main/java/ru/kpfu/itis/genatulin/hw5/services/CalculatorService.java
@@ -3,6 +3,7 @@ package ru.kpfu.itis.genatulin.hw5.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.kpfu.itis.genatulin.hw5.Calculator;
+import ru.kpfu.itis.genatulin.hw5.entities.Calculation;
 import ru.kpfu.itis.genatulin.hw5.entities.Operation;
 import ru.kpfu.itis.genatulin.hw5.repositories.CalculationRepository;
 
@@ -22,15 +23,29 @@ public class CalculatorService {
         this.calculationRepository = calculationRepository;
     }
 
-    public int calculate(int arg1, int arg2, Operation operation) throws NoSuchMethodException {
-        Class<? extends Calculator> calculatorClass = calculator.getClass();
-        try {
-            Method method = calculatorClass.getMethod(operation.name().toLowerCase(Locale.ROOT), Integer.class, Integer.class);
-            Integer result = (Integer) method.invoke(calculator, arg1, arg2);
-            return result;
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            e.printStackTrace();
-            return (Integer) null;
+    public Integer calculate(int arg1, int arg2, Operation operation) throws NoSuchMethodException {
+        Calculation calculation = calculationRepository.getCalculation(arg1, arg2, operation);
+        if (calculation != null) {
+            return calculation.getResult();
+        }
+        else {
+            Class<? extends Calculator> calculatorClass = calculator.getClass();
+            try {
+                Method method = calculatorClass.getMethod(operation.name().toLowerCase(Locale.ROOT), Integer.class, Integer.class);
+                Integer result = (Integer) method.invoke(calculator, arg1, arg2);
+
+                Calculation newCalculation = new Calculation();
+                newCalculation.setArg1(arg1);
+                newCalculation.setArg2(arg2);
+                newCalculation.setOperation(operation);
+                newCalculation.setResult(result);
+                calculationRepository.saveCalculation(newCalculation);
+
+                return result;
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
     }
 
