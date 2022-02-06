1a71e02f8f1106b8e3c94765c6874c2b629368ab
ilnas <genatula@protonmail.com>
2022-02-06 12:44:32 +0300

    Add @RequestMapping

--- hw5/src/main/java/ru/kpfu/itis/genatulin/hw5/controllers/SubtractController.java
+++ hw5/src/main/java/ru/kpfu/itis/genatulin/hw5/controllers/SubtractController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import ru.kpfu.itis.genatulin.hw5.entities.Operation;
 import ru.kpfu.itis.genatulin.hw5.services.CalculatorService;
 
 @Controller
+@RequestMapping(value = "/subtract")
 public class SubtractController {
     private final CalculatorService service;
 
