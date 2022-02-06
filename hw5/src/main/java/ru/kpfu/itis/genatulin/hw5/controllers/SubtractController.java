a5f405e02bcac2c62d762ac221344dc2f86f5d12
ilnas <genatula@protonmail.com>
2022-02-06 12:42:04 +0300

    Write SubtractController

--- hw5/src/main/java/ru/kpfu/itis/genatulin/hw5/controllers/SubtractController.java
+++ hw5/src/main/java/ru/kpfu/itis/genatulin/hw5/controllers/SubtractController.java
@@ -1,7 +1,31 @@
 package ru.kpfu.itis.genatulin.hw5.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.kpfu.itis.genatulin.hw5.entities.Operation;
+import ru.kpfu.itis.genatulin.hw5.services.CalculatorService;
 
 @Controller
 public class SubtractController {
+    private final CalculatorService service;
+
+    @Autowired
+    public SubtractController(CalculatorService service) {
+        this.service = service;
+    }
+
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String getResult(@RequestParam(name = "a") int arg1, @RequestParam(name = "b") int arg2) {
+        try {
+            Integer result = service.calculate(arg1, arg2, Operation.SUBTRACT);
+            return arg1 + " - " + arg2 + " = " + result;
+        } catch (NoSuchMethodException e) {
+            return "Something went wrong";
+        }
+    }
 }
